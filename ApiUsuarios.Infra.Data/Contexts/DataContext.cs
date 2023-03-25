d1a7e2d6ffea9bed0c95a39ce88db391d4d407b6
BetsonAshton <luisrobertoashton@gmail.com>
2023-03-25 16:03:36 -0300

    first commit

---
+++ ApiUsuarios.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using ApiUsuarios.Domain.Entities;
+using ApiUsuarios.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiUsuarios.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Classe de contexto para conexão com o banco de dados
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        //método para conexão com o banco de dados ou InMemory
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseInMemoryDatabase(databaseName: "BD_ApiUsuarios");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BD_ApiUsuarios;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        //método para adicionarmos as classes de mapeamento
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+        }
+
+        //Propriedade DbSet para cada entidade (CRUD)
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto AspNet API para Controle de Usuários
+Treino C# WebDeveloper 
+
+## Tecnologias utilizadas:
+
+* AspNet 7 API
+* Microserviços REST
+* Swagger
+* Entity Framework Core e acesso ao SqlServer(criar banco com o nome BD_ApiUsuarios)
+* DDD(Domain-Driven Design)
+* TDD(Test Driven Development) 
